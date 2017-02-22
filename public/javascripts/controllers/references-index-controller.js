22a77e4d631b5f434cd33fe59cfc9036105893b7
Robin Guo <robin.guo.ch@gmail.com>
2017-02-23 03:47:28 +0800

    add authorization for reference management

--- public/javascripts/controllers/references-index-controller.js
+++ public/javascripts/controllers/references-index-controller.js
@@ -1,7 +1,17 @@
 angular.module("engbooster")
-  .controller("ReferencesIndexController", ["$scope", "References", function($scope, References) {
+  .controller("ReferencesIndexController", ["$scope", "References", "Auth", function($scope, References, Auth) {
     References.all()
       .then(function(res) {
         $scope.references = res.data;
       });
+
+    var token = Auth.getToken();
+    console.log(Auth.parseJwt(token));
+
+    $scope.checkUser = function() {
+      var token = Auth.getToken();
+      var username = Auth.parseJwt(token);
+      // return username === "s.120@qq.com" || username === "admin";
+      return username === "s.120@qq.com";
+    };
   }]);
